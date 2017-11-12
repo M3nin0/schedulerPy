4a74cfeb892a4ad5f9f8f8258792d74d7ef03b71
Phelps <phelpsmemo@gmail.com>
2017-11-12 21:48:10 -0200

    Organização do algoritimo com a interface

--- app.py
+++ app.py
@@ -18,16 +18,18 @@ class Ui_MainWindow(QWidget):
 
     def executa_processos(self):
 
-        if self.prioridade != 0:
+        if self.fila_sistema.count() > 0:
             self.prioridade.run(self.fila_sistema)
-        elif self.interativo != 0:
+
+        if self.fila_interativa.count() > 0:
             self.interativo.run(self.fila_interativa)
-        elif self.batch != 0:
+
+        if self.fila_batch.count() > 0:
             self.batch.run(self.fila_batch)
 
-        # item = self.fila_batch.item(1)
+        # item = self.fila_batch.item(0)
         # item.setBackground(QColor('#2ecc71'))
-
+        # item.setBackground(QColor('#2ecc71'))
         # self.fila_batch.takeItem(0)
 
     def get_info(self):
@@ -71,64 +73,99 @@ class Ui_MainWindow(QWidget):
 
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(860, 490)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.btn_executa = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_executa.setGeometry(QtCore.QRect(20, 320, 141, 71))
+        self.btn_executa.setGeometry(QtCore.QRect(20, 330, 141, 71))
         self.btn_executa.setObjectName("pushButton")
         self.btn_executa.clicked.connect(self.executa_processos)
 
+
+        self.lbl_pronto_2 = QtWidgets.QLabel(self.centralwidget)
+        self.lbl_pronto_2.setGeometry(QtCore.QRect(720, 310, 95, 31))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.lbl_pronto_2.setFont(font)
+        self.lbl_pronto_2.setStyleSheet("")
+        self.lbl_pronto_2.setObjectName("lbl_pronto_2")
         self.splitter = QtWidgets.QSplitter(self.centralwidget)
-        self.splitter.setGeometry(QtCore.QRect(50, 80, 768, 192))
+        self.splitter.setGeometry(QtCore.QRect(50, 40, 751, 29))
         self.splitter.setOrientation(QtCore.Qt.Horizontal)
         self.splitter.setObjectName("splitter")
-        self.fila_sistema = QtWidgets.QListWidget(self.splitter)
+
+        self.btn_sistema = QtWidgets.QPushButton(self.splitter)
+        self.btn_sistema.setObjectName("btn_sistema")
+        self.btn_sistema.clicked.connect(self.add_processo_sistema)
+
+        self.btn_interativo = QtWidgets.QPushButton(self.splitter)
+        self.btn_interativo.setObjectName("btn_interativo")
+        self.btn_interativo.clicked.connect(self.add_processo_interativo)
+
+        self.btn_batch = QtWidgets.QPushButton(self.splitter)
+        self.btn_batch.setObjectName("btn_batch")
+        self.btn_batch.clicked.connect(self.add_processo_batch)
+
+        self.splitter_2 = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter_2.setGeometry(QtCore.QRect(60, 80, 768, 192))
+        self.splitter_2.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter_2.setObjectName("splitter_2")
+        self.fila_sistema = QtWidgets.QListWidget(self.splitter_2)
         font = QtGui.QFont()
         font.setPointSize(14)
         font.setBold(True)
         font.setWeight(75)
         self.fila_sistema.setFont(font)
         self.fila_sistema.setObjectName("fila_sistema")
-
-
-        self.fila_batch = QtWidgets.QListWidget(self.splitter)
-        self.fila_batch.setObjectName("fila_batch")
-
-        self.fila_interativa = QtWidgets.QListWidget(self.splitter)
+        self.fila_interativa = QtWidgets.QListWidget(self.splitter_2)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.fila_interativa.setFont(font)
         self.fila_interativa.setObjectName("fila_interativa")
-
-        self.splitter_2 = QtWidgets.QSplitter(self.centralwidget)
-        self.splitter_2.setGeometry(QtCore.QRect(50, 40, 771, 29))
-        self.splitter_2.setOrientation(QtCore.Qt.Horizontal)
-        self.splitter_2.setObjectName("splitter_2")
-
-        self.btn_sistema = QtWidgets.QPushButton(self.splitter_2)
-        self.btn_sistema.setObjectName("btn_sistema")
-        self.btn_sistema.clicked.connect(self.add_processo_sistema)
-
-        self.btn_batch = QtWidgets.QPushButton(self.splitter_2)
-        self.btn_batch.setObjectName("btn_batch")
-        self.btn_batch.clicked.connect(self.add_processo_batch)
-
-        self.btn_interativo = QtWidgets.QPushButton(self.splitter_2)
-        self.btn_interativo.setObjectName("btn_interativo")
-        self.btn_interativo.clicked.connect(self.add_processo_interativo)
-
+        self.fila_batch = QtWidgets.QListWidget(self.splitter_2)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.fila_batch.setFont(font)
+        self.fila_batch.setObjectName("fila_batch")
+        self.splitter_3 = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter_3.setGeometry(QtCore.QRect(710, 340, 95, 72))
+        self.splitter_3.setOrientation(QtCore.Qt.Vertical)
+        self.splitter_3.setObjectName("splitter_3")
+        self.lbl_pronto = QtWidgets.QLabel(self.splitter_3)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.lbl_pronto.setFont(font)
+        self.lbl_pronto.setStyleSheet("background-color: rgb(52, 152, 219);")
+        self.lbl_pronto.setObjectName("lbl_pronto")
+        self.lbl_espera = QtWidgets.QLabel(self.splitter_3)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.lbl_espera.setFont(font)
+        self.lbl_espera.setStyleSheet("background-color: rgb(243, 156, 18);")
+        self.lbl_espera.setTextFormat(QtCore.Qt.RichText)
+        self.lbl_espera.setObjectName("lbl_espera")
+        self.lbl_executando = QtWidgets.QLabel(self.splitter_3)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.lbl_executando.setFont(font)
+        self.lbl_executando.setStyleSheet("background-color: rgb(46, 204, 113);")
+        self.lbl_executando.setObjectName("lbl_executando")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 860, 23))
         self.menubar.setObjectName("menubar")
-        self.menuInicio = QtWidgets.QMenu(self.menubar)
-        self.menuInicio.setObjectName("menuInicio")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.actionSobre = QtWidgets.QAction(MainWindow)
         self.actionSobre.setObjectName("actionSobre")
-        self.menuInicio.addAction(self.actionSobre)
-        self.menubar.addAction(self.menuInicio.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -137,11 +174,14 @@ class Ui_MainWindow(QWidget):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Multiplas filas"))
         self.btn_executa.setText(_translate("MainWindow", "Iniciar processador"))
+        self.lbl_pronto_2.setText(_translate("MainWindow", "Legenda"))
         self.btn_sistema.setToolTip(_translate("MainWindow", "<html><head/><body><p><br/></p></body></html>"))
         self.btn_sistema.setText(_translate("MainWindow", "Adicionar processo de sistema"))
-        self.btn_batch.setText(_translate("MainWindow", "Adicionar processo batch"))
         self.btn_interativo.setText(_translate("MainWindow", "Adicionar processo interativo"))
-        self.menuInicio.setTitle(_translate("MainWindow", "Inicio"))
+        self.btn_batch.setText(_translate("MainWindow", "Adicionar processo batch"))
+        self.lbl_pronto.setText(_translate("MainWindow", "Pronto"))
+        self.lbl_espera.setText(_translate("MainWindow", "Em espera"))
+        self.lbl_executando.setText(_translate("MainWindow", "Executando"))
         self.actionSobre.setText(_translate("MainWindow", "Sobre"))
 
 
--- utils/fila.py
+++ utils/fila.py
@@ -7,70 +7,94 @@ Classe que representa o escalonamento circular
 # Pronto: #3498db
 # Em espera: #f39c12
 
-from utils.processo import Processo
 from time import sleep
+from PyQt5.QtGui import *
+from utils.processo import Processo
 
 class Circular():
-    def __init__(self, prioridade, qtd_processos, tempo_limite):
+    def __init__(self, prioridade, qtd_processos, quantum):
         self._prioridade = prioridade
-        self.fila = {}
+        self.fila = []
         self._total = 0
         self.qtd_processos = qtd_processos
         # Tempo limite de cada execução
-        self.tempo_limite = tempo_limite
+        self.quantum = quantum
         self.cores = {'executando': '#2ecc71', 'pronto': '#3498db', 'espera': '#f39c12'}
+
+    def refresh(self,lista):
+        lista.update()
+        lista.repaint()
 
     @property
     def prioridade(self):
         return self._prioridade
 
     def add(self, processo):
-        # self.fila.append(processo)
-        self.fila[processo.id] = processo
+        self.fila.append(processo)
+        # self.fila[processo.id] = processo
 
-    def remove(self, processo):
-        # self.fila.remove(processo)
-        del self.fila[processo.id]
+    def remove(self, processo, lista):
+        lista.takeItem(0)
+        self.fila.remove(processo)
+        self.refresh(lista)
+        # del self.fila[processo.id]
 
-    def run(self, label):
+        # item = self.fila_batch.item(1)
+        # item.setBackground(QColor('#2ecc71'))
+
+        # self.fila_batch.takeItem(0)
+
+    def run(self, lista):
         while len(self.fila) != 0:
             for p in range(0, len(self.fila)):
                 c = 0
-                print('Processo sendo executado: ID ' + str(self.fila[p].id))
-                while c < self.tempo_limite:
-                    sleep(2)
+
+                item = lista.item(0)
+                item.setBackground(QColor(self.cores['executando']))
+                lista.insertItem(0, item)
+                self.refresh(lista)
+
+                while c < self.quantum:
+                    print('Rodando: ' + str(self.fila[p].id))
+                    sleep(1)
                     self.fila[p].tempo -= 1
                     c += 1
                 if self.fila[p].tempo <= 0:
-                    print('Processo finalizado: ID ' + str(self.fila[p].id))
-                    self.fila.remove(self.fila[p])
+                    print('Terminado')
+                    self.remove(self.fila[p], lista)
                 else:
-                    print('Processo entrou em espera: ID ' + str(self.fila[p].id))
+                    print('Parando: ' + str(self.fila[p].id))
+                    lista.takeItem(0)
+                    item.setBackground(QColor(self.cores['pronto']))
+                    lista.addItem(item)
+                    self.refresh(lista)
                     temp = self.fila[p]
                     self.fila.remove(self.fila[p])
                     self.fila.append(temp)
 
 class Prioridade():
-    def __init__(self, prioridade, qtd_processos, tempo_limite):
+    def __init__(self, prioridade, qtd_processos, quantum):
         self._prioridade = prioridade
-        self.fila = {}
+        self.fila = []
         self._total = 0
         self.qtd_processos = qtd_processos
         # Tempo limite de cada execução
-        self.tempo_limite = tempo_limite
+        self.quantum = quantum
         self.cores = {'executando': '#2ecc71', 'pronto': '#3498db', 'espera': '#f39c12'}
+
+    def refresh(self,lista):
+        lista.update()
+        lista.repaint()
 
     @property
     def prioridade(self):
         return self._prioridade
 
     def add(self, processo):
-        # self.fila.append(processo)
-        self.fila[processo.id] = processo
+        self.fila.append(processo)
+        # self.fila[processo.id] = processo
 
-    def remove(self, processo):
-        # self.fila.remove(processo)
-        del self.fila[processo.id]
-
-    def run(self, label):
-        pass
+    def remove(self, processo, lista):
+        lista.takeItem(0)
+        self.fila.remove(processo)
+        self.refresh(lista)
